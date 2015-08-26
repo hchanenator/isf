6aad359535d9042f2f2e5a685a4066199a06d782
HChan <hchanenator@gmail.com>
2015-08-26 15:45:33 -0400

    Add scope annotations

--- src/main/java/com/apress/isf/spring/annotated/service/AnnotatedSearchEngine.java
+++ src/main/java/com/apress/isf/spring/annotated/service/AnnotatedSearchEngine.java
@@ -7,7 +7,10 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import com.apress.isf.java.model.Document;
@@ -20,21 +23,24 @@ import com.apress.isf.spring.data.DocumentDAO;
  *
  */
 @Service("engine")
+@Scope("prototype")
 public class AnnotatedSearchEngine implements SearchEngine {
- 
-        @Autowired
-        private DocumentDAO documentDAO;
-         
-        public List<Document> findByType(Type documentType) {
-                List<Document>  result = new ArrayList<Document>();
-                for(Document doc : listAll()){
-                        if(doc.getType().getName().equals(documentType.getName()))
-                                result.add(doc);
-                }
-                return result;
-        }
-         
-        public List<Document> listAll() {
-                return Arrays.asList(documentDAO.getAll());
-        }
+
+	private static final Logger log = LoggerFactory.getLogger(AnnotatedSearchEngine.class);
+	
+	@Autowired
+	private DocumentDAO documentDAO;
+
+	public List<Document> findByType(Type documentType) {
+		List<Document> result = new ArrayList<Document>();
+		for (Document doc : listAll()) {
+			if (doc.getType().getName().equals(documentType.getName()))
+				result.add(doc);
+		}
+		return result;
+	}
+
+	public List<Document> listAll() {
+		return Arrays.asList(documentDAO.getAll());
+	}
 }
